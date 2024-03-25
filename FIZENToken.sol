31feba30189bd43c367cef6bdb428ac22bc9684e
Fizen-GMBH <160200240+Fizenvinhvu@users.noreply.github.com>
2024-03-25 15:44:14 +0700

    Create FIZENToken.sol
    Contract for the issuance of FIZEN Tokens

---
+++ FIZENToken.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier:MIT
+pragma solidity 0.8.0;
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract FIZENToken is  ERC20, Ownable {
+
+}
